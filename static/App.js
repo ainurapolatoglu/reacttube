7253e1daa1959724d5c20960e5a31cb8826780bd
ainurapolatoglu <ainura.dyuss@gmail.com>
2020-05-25 23:08:04 +0200

    Refactoring

---
+++ static/App.js
@@ -0,0 +1,30 @@
+//React core
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+//Assets and style
+import logo from './logo.svg';
+import './App.css';
+
+//Components
+import HomePage from './components/templates/HomePage'
+import ResultsPage from './components/templates/ResultsPage'
+import VideoPage from './components/templates/VideoPage'
+
+// Style
+import './style/style.css'
+
+export default function App() {
+  // Render
+  return (
+    <Router>
+      <main className='App'>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/results/:query' component={ResultsPage} />
+          <Route path='/video/:id' component={VideoPage} />
+        </Switch>
+      </main>
+    </Router>
+  )
+}
